3da4a21d954450de636ee155565ea120bc368305
h_ash <lm314v21@sk9.sakura.ne.jp>
2020-09-11 10:32:14 +0900

    コードチェック

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   end
 
   def render_500(e)
-    ExceptionNotifier.notify_exception(e, env: request.env, data: {message: "error"})
+    ExceptionNotifier.notify_exception(e, env: request.env, data: { message: "error" })
     respond_to do |format|
       format.html { render template: 'front/errors/500', layout: 'front/layouts/error', status: 500 }
       format.all { render nothing: true, status: 500 }
