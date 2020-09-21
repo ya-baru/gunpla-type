1d7c042ab288402e084460076a0e58f89e006398
h_ash <lm314v21@sk9.sakura.ne.jp>
2020-09-22 01:16:38 +0900

    sessions request specを作成

---
+++ spec/requests/users/sessions_spec.rb
@@ -0,0 +1,86 @@
+require 'rails_helper'
+
+RSpec.describe "Users::Sessions", type: :request do
+  subject { response }
+
+  let(:user) { create(:user) }
+
+  describe "#new" do
+    before do
+      login
+      get new_user_session_path
+    end
+
+    context "ログインユーザー" do
+      let(:login) { sign_in user }
+
+      it { is_expected.to have_http_status(302) }
+      it { is_expected.to redirect_to users_profile_path(user) }
+    end
+
+    context "未ログインユーザー" do
+      let(:login) { nil }
+
+      it { is_expected.to have_http_status(200) }
+    end
+  end
+
+  describe "#create" do
+    before do
+      login
+      post user_session_path, params: {
+        user: { email: user.email, password: user.password },
+      }
+    end
+
+    context "ログインユーザー" do
+      let(:login) { sign_in user }
+
+      it { is_expected.to have_http_status(302) }
+      it { is_expected.to redirect_to users_profile_path(user) }
+    end
+
+    context "未ログインユーザー" do
+      let(:login) { nil }
+
+      it { is_expected.to have_http_status(302) }
+      it { is_expected.to redirect_to users_profile_path(user) }
+    end
+
+    context "アカウント凍結ユーザー" do
+      let(:user) { create(:user, :account_lock) }
+      let(:login) { nil }
+
+      it { is_expected.to have_http_status(200) }
+    end
+
+    context "アカウント未有効化ユーザー" do
+      let(:user) { create(:user, :unconfirmation) }
+      let(:login) { nil }
+
+      it { is_expected.to have_http_status(302) }
+      it { is_expected.to redirect_to new_user_session_path }
+    end
+  end
+
+  describe "#destroy" do
+    before do
+      login
+      delete destroy_user_session_path
+    end
+
+    context "ログインユーザー" do
+      let(:login) { sign_in user }
+
+      it { is_expected.to have_http_status(302) }
+      it { is_expected.to redirect_to root_path }
+    end
+
+    context "未ログインユーザー" do
+      let(:login) { nil }
+
+      it { is_expected.to have_http_status(302) }
+      it { is_expected.to redirect_to root_path }
+    end
+  end
+end
