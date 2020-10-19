b41e4db0ceabf9c534bfbc8a4491370bed7f7e8f
h_ash <lm314v21@sk9.sakura.ne.jp>
2020-10-19 10:34:19 +0900

    raty関連ファイルをwebpackに移動

--- app/controllers/users/reviews_controller.rb
+++ app/controllers/users/reviews_controller.rb
@@ -1,26 +1,18 @@
 class Users::ReviewsController < ApplicationController
-  before_action :authenticate_user!, only: %i(new create edit update)
+  before_action :authenticate_user!, only: %i(new create edit update destroy)
+  before_action :set_review, only: %i(new create)
 
   def show
     @review = Review.find(params[:id])
     @gunpla = @review.gunpla
   end
 
-  def new
-    @gunpla = Gunpla.find(params[:gunpla_id])
-    @review = current_user.reviews.build(gunpla_id: @gunpla.id).decorate
-  end
+  def new; end
 
   def create
-    @gunpla = Gunpla.find(params[:gunpla_id])
-    @review = current_user.reviews.build(review_params).decorate
+    return render :new unless @review.save
 
-    if @review.save
-      flash[:notice] = "投稿が完了しました。"
-      redirect_to gunpla_url(@gunpla)
-    else
-      render "new"
-    end
+    redirect_to gunpla_url(@gunpla), notice: "『#{@gunpla.name}』のレビューが完了しました。"
   end
 
   def edit
@@ -36,5 +28,14 @@ class Users::ReviewsController < ApplicationController
 
   def review_params
     params.require(:review).permit(:title, :content, :rate, :gunpla_id, images: [])
+  end
+
+  def set_review
+    @gunpla = Gunpla.find(params[:gunpla_id])
+    if Review.exists?(user_id: current_user.id, gunpla_id: @gunpla.id)
+      redirect_to gunpla_path(@gunpla), alert: "このガンプラはレビュー済みです"
+      return
+    end
+    @review = current_user.reviews.build(gunpla_id: @gunpla.id).decorate
   end
 end
