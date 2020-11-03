6880d448456d990f2db3471ec8923ebaf6a43cfe
h_ash <lm314v21@sk9.sakura.ne.jp>
2020-11-03 13:26:32 +0900

    トップページのレイアウト作成

--- app/controllers/users/home_controller.rb
+++ app/controllers/users/home_controller.rb
@@ -1,3 +1,7 @@
 class Users::HomeController < ApplicationController
-  def index; end
+  def index
+    @articles = Article.limit(10).order(created_at: "desc").decorate
+    @gunplas = Gunpla.includes([:category, :reviews]).order(favorites_count: "desc").limit(RANK_LIST_COUNT).decorate
+    @reviews = Review.limit(5).order(created_at: "desc").decorate
+  end
 end
