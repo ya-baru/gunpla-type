0765c887aa6deca7c8ac2cb2ecf19fdbb64f04c0
h_ash <lm314v21@sk9.sakura.ne.jp>
2020-11-04 14:56:56 +0900

    circleciテスト

--- spec/requests/users/home_spec.rb
+++ spec/requests/users/home_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Users::Homes", type: :request do
+RSpec.describe "Users::Homes", :focus, type: :request do
   describe "#index" do
     subject { response }
 
-    before { get root_path }
+    before { get root_url }
 
     it { is_expected.to have_http_status 200 }
   end
